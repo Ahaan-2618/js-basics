74cc5a80e90158db8800ce75584190de3c44a6f1
Ahaan Soni <kartiksoni0008@gmail.com>
2023-12-04 22:15:12 +0530

    Basics of JS

---
+++ basics.js
@@ -0,0 +1,6 @@
+const userId = 144553;
+let userName = "Ahaan" // 
+var userEmail = "random@test.com" // block and functional scope issue
+let userState; // undefined
+
+console.table([userId, userEmail, userName]);
---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+"use-strict" // treat all JS code as newer version
+
+
+// Primitive
+const name = "Name";
+const age = 18;
+const isAdult = true;
+const isMarried = null; // console.log(typeof null); will be object 
+let education; // undefined // const can not be used as undefined
+const bigNum = 123422222222222222222222222222222222222n; // bigInt
+const sym = Symbol("Hello") // Symbol
+
+// Non-primitive 
+
+
+
+
+
+console.log(typeof sym);
+console.log(typeof bigNum);
