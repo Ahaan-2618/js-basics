fcc59bcfd87e85863f9491684c9f7bd3700f1270
Ahaan Soni <kartiksoni0008@gmail.com>
2023-12-05 22:55:18 +0530

    memory in JS

---
+++ memoryInJs.js
@@ -0,0 +1,20 @@
+// Memory Types
+// stack -> Primitive 9 Copy of original value )
+// heap -> Non - Primitive ( Reference of original value )
+
+let userName = "Ahaan Soni";
+console.log(userName); // Ahaan Soni
+
+let userNamecopy = userName;
+userNamecopy = "Kartik Soni";
+console.log(userNamecopy); // kartik Soni // Because of copy this got changed and primary value remain same
+
+let userObj = {
+  email: "Sample@gmail.com",
+  name: "Kartik",
+};
+
+let userObj1 = userObj;
+userObj1.email = "Google@gmail.com";
+
+console.table([userObj, userObj1]); // provided refernce and both values got changed
