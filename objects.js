6e03775ba58590f922d9c51dc767fbe0a8a5a697
Ahaan Soni <kartiksoni0008@gmail.com>
2023-12-14 21:29:02 +0530

    Added file for object

---
+++ objects.js
@@ -0,0 +1,23 @@
+// singleton
+// object literals
+
+const mySym = Symbol("Key1");
+
+const myObj = {
+  name: "Ahaan",
+  age: 18,
+  location: "Jaipur",
+  fullName: "Ahaan Soni", // we can not access this value by ( . ) so square notation will be used
+  [mySym]: "myKey1",
+};
+
+console.table([myObj.age, myObj["age"], myObj["fullName"]]); // in 2nd type key is default string so we can access like this as well.
+console.log(typeof myObj[mySym]);
+
+myObj.greetings = function () {
+  console.log(`Hello ${this.name}`); // string interpolation // use this for current function property
+};
+
+console.log(myObj.greetings());
+
+Object.freeze(myObj); // to make immutable
