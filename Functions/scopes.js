5ed4e5670f934dd90c9907d1f95ebfc884aa7a0f
Ahaan Soni <kartiksoni0008@gmail.com>
2023-12-18 19:50:09 +0530

    JS Scopes

--- Functions/scopes.js
+++ Functions/scopes.js
@@ -1,3 +1,26 @@
 var a = 1; // global scope
 let b = 2; // block scope
 const c = 3; // block scope
+
+function one() {
+  const userName = "Ahaan";
+
+  function two() {
+    const website = "Youtube";
+    console.log(userName);
+  }
+  two();
+  // console.log(website);
+}
+
+one();
+
+console.log(addOne(5)); // hoisting worked
+function addOne(num) {
+  return num + 1;
+}
+
+console.log(addTwo(2)); // this will give error // using before declaration
+const addTwo = function addThree(num) {
+  return num + 3;
+};
